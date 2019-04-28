f858537f4ee6a890ab5034cf6df407cae0cb2d2b
wfortini <wpnfortini@gmail.com>
2019-04-28 02:55:09 -0300

    remove node_modules

--- shopList/src/index.ts
+++ shopList/src/index.ts
@@ -1,8 +1,11 @@
+import { normalizePort, onError, onListening } from './utils/utils';
 import * as http from 'http';
 
 import app from './app';
 
 const server = http.createServer(app);
+const port = normalizePort(process.env.port || 3000);
 
-server.listen(3000);
-server.on('listenig', () => console.log('listenig on port 3000...'));
+server.listen(port);
+server.on('error', onError(server));
+server.on('listening', onListening(server));
