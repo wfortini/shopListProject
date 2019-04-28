82b5352af4989d6674c4bf69a57b1f0ab6ece72d
wfortini <wpnfortini@gmail.com>
2019-04-28 03:38:58 -0300

    postgree

--- shopList/dist/index.js
+++ shopList/dist/index.js
@@ -1,7 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = require("./utils/utils");
 const http = require("http");
 const app_1 = require("./app");
 const server = http.createServer(app_1.default);
-server.listen(3000);
-server.on('listenig', () => console.log('listenig on port 3000...'));
+const port = utils_1.normalizePort(process.env.port || 3000);
+server.listen(port);
+server.on('error', utils_1.onError(server));
+server.on('listening', utils_1.onListening(server));
