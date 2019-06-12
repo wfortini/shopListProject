f317292cd0998b6733a09f85fc56394ff76a2440
wfortini <wpnfortini@gmail.com>
2019-06-12 00:15:09 -0300

    dependencias

--- shopListMobile/src/App.js
+++ shopListMobile/src/App.js
@@ -5,9 +5,19 @@ import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import Routes from './Routes';
 import ReduxThunk from 'redux-thunk';
+import ApolloProvider from 'react-apollo';
+import ApolloClient from 'apollo-client';
+import HttpLink from 'apollo-link-http';
+import InMemoryCache from 'apollo-cache-inmemory';
 
+const apolloClient = new ApolloClient({
+    link: new HttpLink({url: 'localhost:'}),
+    cache: new InMemoryCache()
+})
 export default props => (
     <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
-        <Routes />
+        <ApolloProvider client={apolloClient} >
+             <Routes />
+        </ApolloProvider>
     </Provider>
 );
