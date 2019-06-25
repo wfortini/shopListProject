d8e2048c93398b24cdac45c0c7ff4fe0522210c6
wfortini <wpnfortini@gmail.com>
2019-06-25 14:51:57 -0300

    firebase admin

--- shopList/src/controllers/authController.ts
+++ shopList/src/controllers/authController.ts
@@ -1,7 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import * as admin from 'firebase-admin';
 
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
 export class AuthController  {
+   
+     
+    constructor(){
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            databaseURL: "https://shoplist-664da.firebaseio.com"
+            });
+    }
+    
 
     public autenticarJWT(req: Request, res: Response, next: NextFunction) {
 
