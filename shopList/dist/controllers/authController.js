b963dd4b4c36227cf2923fed308dfa80c15267ec
wfortini <wpnfortini@gmail.com>
2019-06-27 22:49:36 -0300

    firebase config

--- shopList/dist/controllers/authController.js
+++ shopList/dist/controllers/authController.js
@@ -1,14 +1,39 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const admin = require("firebase-admin");
+var serviceAccount = require("/home/wellington/projetoNode/shoplist-664da-firebase-adminsdk-wic97-c4ce212cee.json");
 class AuthController {
+    constructor() {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            databaseURL: "https://shoplist-664da.firebaseio.com"
+        });
+    }
     autenticarJWT(req, res, next) {
         let authorization = req.get('authorization');
         let token = authorization ? authorization.split(' ')[1] : undefined;
-        if (token) {
-            return res.status(401).json({ status: "error", code: "unauthorized" });
+        if (token !== undefined) {
+            console.log(`  token ${token}`);
+            admin.auth().verifyIdToken(token)
+                .then(function (decodedToken) {
+                var uid = decodedToken.uid;
+                console.log(`  uid ${uid}`);
+                req.params.uid = uid;
+                next();
+            }).catch(function (error) {
+                var code = error.code;
+                if (code === 'auth/id-token-expired') {
+                    return res.status(400).json({ status: "error", code: "token-expirad" });
+                }
+                else if (code === 'auth/argument-erro') {
+                    return res.status(400).json({ status: "error", code: "token-invalid" });
+                }
+                console.log(error);
+                return res.status(401).json({ status: "error", code: "unauthorized" });
+            });
         }
         else {
-            next();
+            return res.status(401).json({ status: "error", code: "unauthorized" });
         }
     }
 }
--- shopList/src/controllers/authController.ts
+++ shopList/src/controllers/authController.ts
@@ -1,29 +1,43 @@
 import { NextFunction, Request, Response } from "express";
 import * as admin from 'firebase-admin';
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("/home/wellington/projetoNode/shoplist-664da-firebase-adminsdk-wic97-c4ce212cee.json");
 
-export class AuthController  {
-   
+export class AuthController  {   
      
     constructor(){
         admin.initializeApp({
             credential: admin.credential.cert(serviceAccount),
             databaseURL: "https://shoplist-664da.firebaseio.com"
             });
-    }
-    
+    }    
 
     public autenticarJWT(req: Request, res: Response, next: NextFunction) {
 
         let authorization: string = req.get('authorization');
         let token: string = authorization ? authorization.split(' ')[1] : undefined;
-
-        if(token){
-            return res.status(401).json({ status: "error", code: "unauthorized" });
+        
+        if(token !== undefined ){
+            console.log(`  token ${token}`);
+            admin.auth().verifyIdToken(token)
+            .then(function(decodedToken) {
+              var uid = decodedToken.uid;
+              console.log(`  uid ${uid}`);
+              req.params.uid = uid;
+              next();
+            }).catch(function(error) {
+                var code = error.code;
+                if(code === 'auth/id-token-expired'){
+                    return res.status(400).json({ status: "error", code: "token-expirad" });
+                }else if(code === 'auth/argument-erro'){
+                    return res.status(400).json({ status: "error", code: "token-invalid" }); 
+                }
+                console.log(error);
+                return res.status(401).json({ status: "error", code: "unauthorized" });
+            });           
 
         }else{
-            next();
+            return res.status(401).json({ status: "error", code: "unauthorized" });
         }
 
 
